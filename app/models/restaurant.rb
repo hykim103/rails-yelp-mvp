44393a1f4c2c6bbee9841cad05479b9102b389e3
HaYeon Kim <kimhy8376@gmail.com>
2022-08-24 14:30:48 +0100

    Create a copycat of yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  CATEGORY = %w[Chinese Italian Japanese French Belgian]
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: "My Old Place", address: "88-90 Middlesex St, London E1 7EZ", phone_number: "020 7247 2200", category: "Chinese")
+Restaurant.create(name: "Duck & Waffle", address: "110 Bishopsgate, London EC2N 4AY", phone_number: "020 3640 7310", category: "Belgian")
+Restaurant.create(name: "Da Mario", address: "15 Gloucester Rd, South Kensington, London SW7 4PP", phone_number: "020 7584 9078", category: "Italian")
+Restaurant.create(name: "Brasserie Zédel", address: "20 Sherwood St, London W1F 7ED", phone_number: "020 7734 4888", category: "French")
+Restaurant.create(name: "Eat Tokyo", address: "50 Red Lion St, High Holborn, London WC1R 4PF", phone_number: "020 7242 3490", category: "Japanese")
